e6c1322ee62e3b2a0b015e3570e2caeb933d8c60
Péli Mátyás <PeliMatyas@pg.local>
2026-01-16 12:17:04 +0100

    Végső

--- features/even.feature
+++ features/even.feature
@@ -1,5 +1,3 @@
-Feature: Páros szám ellenőrzése
-
   # TODO: Írd ide a scenariókat!
   # 1. Páros szám ellenőrzése (pl. 4)
   # 2. Páratlan szám ellenőrzése (pl. 5)
@@ -7,7 +5,20 @@ Feature: Páros szám ellenőrzése
   # 4. Negatív páros szám ellenőrzése (-4)
   # 5. Negatív páratlan szám ellenőrzése (-5)
 
-  Scenario: Páros szám ellenőrzése
-  Given the number is 4
-  When I check the number 
-  Then I should be told "even"
+
+Feature: Is it even/odd?
+
+Scenario Outline: The number is even or odd
+  Given the number is "<number>"
+  When I ask whether it's even or odd
+  Then I should be told "<answer>"
+
+Examples:
+  | number | answer |
+  | 4 | even |
+  | 5 | odd |
+  | 0 | even |
+  | -4 | even |
+  | -5 | odd |
+
+
--- features/steps/even_steps.py
+++ features/steps/even_steps.py
@@ -3,19 +3,30 @@ from behave import given, when, then
 from src.number_checker import check_number
 
 # TODO: Implementáld a Given step-et
-@given('the number is 4')
-def step_given_number(context):
-    context.number = 4
+#@given('the number is 4')
+#def step_given_number(context):
+#    context.number = 4
 
+#@given('the number is 5')
+#def step_given_number(context):
+#    context.number = 5
+
+#@given('the number is 0')
+#def step_given_number(context):
+#    context.number = 0
+
+@given('the number is "{number}"')
+def step_given_number(context, number):
+    context.number = int(number)
 
 # TODO: Implementáld a When step-et
 # Használd a check_number függvényt a src/number_checker.py fájlból!
-@when('I check the number')
+@when("I ask whether it's even or odd")
 def step_when_check_number(context):
     context.result = check_number(context.number)
 
 
 # TODO: Implementáld a Then step-et
-@then('I should be told "{expected}"')
-def step_then_result(context, expected):
-    assert context.result == expected, f'Elvárt érték: "{expected}" de a kapott érték: "{context.result}"'
+@then('I should be told "{expected_answer}"')
+def step_when_I_ask_whether_its_even_or_odd(context, expected_answer):
+    assert context.result == expected_answer
